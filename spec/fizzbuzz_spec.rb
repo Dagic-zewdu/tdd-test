4dbac9f66d0b7b15d3f38e56eb84f0da7073cdc2
Thermiee <navyblack16@yahoo.com>
2022-05-19 22:24:12 +0100

    fix rspec errors

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,28 +4,28 @@ describe Solver do
   context 'the input is 15 (divisible by 5 and 3)' do
     it 'should return fizzbuzz' do
       solver = Solver.new
-      expect(solver.fizzbuzz(15)).to eq 'fizzbuzz'
+      expect(solver.fizz_buzz(15)).to eq 'FizzBuzz'
     end
   end
 
   context 'the input is 6 (divisible by 3)' do
     it 'should return fizz' do
       solver = Solver.new
-      expect(solver.fizzbuzz(6)).to eq 'fizz'
+      expect(solver.fizz_buzz(6)).to eq 'Fizz'
     end
   end
 
   context 'the input is 5 (divisible by 5)' do
     it 'should return buzz' do
       solver = Solver.new
-      expect(solver.fizzbuzz(5)).to eq 'buzz'
+      expect(solver.fizz_buzz(5)).to eq 'Buzz'
     end
   end
 
   context 'the input is 7 (not divisible by 5 or 3)' do
     it 'should return 7' do
       solver = Solver.new
-      expect(solver.fizzbuzz(7)).to eq '7'
+      expect(solver.fizz_buzz(7)).to eq '7'
     end
   end
 end
